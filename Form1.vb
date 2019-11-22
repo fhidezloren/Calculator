d5c375de08c3608aa5cb0f0341f2b812a69ad954
fhidezloren <fhidezloren@gmail.com>
2019-11-22 23:19:57 +0800

    Calculator

---
+++ Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+7
+    End Sub
+End Class
